035b9096026e8e3bcd59f92ad8c16036cbd149ba
Panagiotis <p.kontoeidis@logicea.com>
2024-07-24 21:40:52 +0300

    first push

---
+++ backend/src/main/java/backend/connectin/ConnectInApplication.java
@@ -0,0 +1,13 @@
+package backend.connectin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConnectInApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConnectInApplication.class, args);
+    }
+
+}
