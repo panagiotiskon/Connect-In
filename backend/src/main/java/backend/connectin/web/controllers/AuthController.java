5cbbb936cda0d37ab6ca69a1fbbca57d76ac3f1d
Stelios Dimitriadis <s.dimitriadis@logicea.com>
2024-08-14 01:36:52 +0300

    added cross origin annotation

--- backend/src/main/java/backend/connectin/web/controllers/AuthController.java
+++ backend/src/main/java/backend/connectin/web/controllers/AuthController.java
@@ -13,14 +13,12 @@ import org.springframework.security.authentication.UsernamePasswordAuthenticatio
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
 
 @RestController
 @RequestMapping("api/auth")
+@CrossOrigin(origins = "http://localhost:3000", allowCredentials = "true")
 public class AuthController {
 
     private final AuthenticationManager authenticationManager;
