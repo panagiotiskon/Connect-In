e46d19923208e7f50268dd2ca70aad3236be532c
Panagiotis <p.kontoeidis@logicea.com>
2024-08-14 10:40:20 +0300

    settings controller

---
+++ backend/src/main/java/backend/connectin/web/controllers/SettingsController.java
@@ -0,0 +1,12 @@
+package backend.connectin.web.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("api/auth")
+public class SettingsController {
+
+
+
+}
