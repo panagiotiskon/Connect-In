325ab21ae243717615c340264e6639a87b2bde8d
Stelios Dimitriadis <s.dimitriadis@logicea.com>
2024-08-14 11:07:20 +0300

    registered a user succesfully

--- frontend/src/components/RegisterComponent.js
+++ frontend/src/components/RegisterComponent.js
@@ -1,319 +1,243 @@
-import React, { useState, useRef } from "react";
-import AuthService from "../api/AuthenticationAPI";
-import { useNavigate } from "react-router-dom";
-import ConnectInLogo from "../assets/ConnectIn.png";
-import { isEmail } from "validator";
-import Form from "react-validation/build/form";
-import "./RegisterComponent.scss";
-import register from "../assets/register.svg";
-import PhotoUpload from "./PhotoUpload"; // Import the PhotoUpload component
-
+import React, { useState } from "react";
 import {
-  MDBBtn,
   MDBContainer,
+  MDBInput,
+  MDBBtn,
+  MDBIcon,
   MDBRow,
   MDBCol,
-  MDBIcon,
-  MDBInput,
+  MDBSpinner,
 } from "mdb-react-ui-kit";
+import { useNavigate } from "react-router-dom";
+import { useForm } from "react-hook-form";
+import AuthService from "../api/AuthenticationAPI";
+import ConnectInLogo from "../assets/ConnectIn.png";
+import register from "../assets/register.svg";
+import PhotoUpload from "./PhotoUpload";
+import "./RegisterComponent.scss";
 
-const requiredField = (value) => {
-  if (!value) {
-    return (
-      <div className="invalid-feedback d-block">This field is required!</div>
-    );
-  }
-};
-
-const validEmail = (value) => {
-  if (!isEmail(value)) {
-    return (
-      <div className="invalid-feedback d-block">This is not a valid email.</div>
-    );
-  }
-};
-
-const validName = (value) => {
-  if (value.length < 3 || value.length > 20) {
-    return (
-      <div className="invalid-feedback d-block">
-        Name must be between 3 and 20 characters.
-      </div>
-    );
-  }
-};
-
-const validSurname = (value) => {
-  if (value.length < 3 || value.length > 20) {
-    return (
-      <div className="invalid-feedback d-block">
-        Surname must be between 3 and 20 characters.
-      </div>
-    );
-  }
-};
-
-const validPassword = (value) => {
-  if (value.length < 6 || value.length > 20) {
-    return (
-      <div className="invalid-feedback d-block">
-        The password must be between 6 and 20 characters.
-      </div>
-    );
-  }
-};
-
-const validPhoneNumber = (value) => {
-  if (value.length !== 10) {
-    return (
-      <div className="invalid-feedback d-block">
-        Phone number must contain 10 numbers.
-      </div>
-    );
-  }
-};
-
-const RegisterComponent = (props) => {
-  const form = useRef();
-  const checkBtn = useRef();
-  const navigate = useNavigate();
-
-  const [name, setName] = useState("");
-  const [surname, setSurname] = useState("");
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-  const [repeatPassword, setRepeatPassword] = useState("");
-  const [phoneNumber, setPhoneNumber] = useState("");
-  const [successful, setSuccessful] = useState(false);
+const RegisterComponent = () => {
+  const {
+    register: formRegister,
+    handleSubmit,
+    formState: { errors },
+    watch,
+  } = useForm();
+  const [loading, setLoading] = useState(false);
   const [message, setMessage] = useState("");
-  const [passwordMatchError, setPasswordMatchError] = useState("");
+  const navigate = useNavigate();
   const [photo, setPhoto] = useState(null);
 
-  const onChangeName = (e) => {
-    const name = e.target.value;
-    setName(name);
-  };
+  const onSubmit = (data) => {
+    setMessage("");
+    setLoading(true);
 
-  const onChangeEmail = (e) => {
-    const email = e.target.value;
-    setEmail(email);
-  };
-
-  const onChangePassword = (e) => {
-    const password = e.target.value;
-    setPassword(password);
-    validatePasswordMatch(password, repeatPassword);
-  };
-
-  const onChangeRepeatPassword = (e) => {
-    const repeatPassword = e.target.value;
-    setRepeatPassword(repeatPassword);
-    validatePasswordMatch(password, repeatPassword);
-  };
-
-  const validatePasswordMatch = (password, repeatPassword) => {
-    if (password && repeatPassword && password !== repeatPassword) {
-      setPasswordMatchError("Passwords do not match.");
-    } else {
-      setPasswordMatchError("");
+    if (data.password !== data.repeatPassword) {
+      setMessage("Passwords do not match.");
+      setLoading(false);
+      return;
     }
-  };
 
-  const onChangeSurname = (e) => {
-    const surname = e.target.value;
-    setSurname(surname);
-  };
+    AuthService.register(
+      data.email,
+      data.name,
+      data.surname,
+      data.password,
+      data.phoneNumber,
+      photo
+    ).then(
+      () => {
+        navigate("/home");
+        window.location.reload();
+      },
+      (error) => {
+        const resMessage =
+          (error.response &&
+            error.response.data &&
+            error.response.data.message) ||
+          error.message ||
+          error.toString();
 
-  const onChangePhoneNumber = (e) => {
-    const phoneNumber = e.target.value;
-    setPhoneNumber(phoneNumber);
+        setLoading(false);
+        setMessage(resMessage);
+      }
+    );
   };
 
   const handleFileUpload = (file) => {
     setPhoto(file);
   };
 
-  const handleRegister = (e) => {
-    e.preventDefault();
-
-    setMessage("");
-    setSuccessful(false);
-
-    form.current.validateAll();
-
-    if (
-      checkBtn.current &&
-      checkBtn.current.context &&
-      checkBtn.current.context._errors.length === 0
-    ) {
-      if (password !== repeatPassword) {
-        setMessage("Passwords do not match.");
-        setSuccessful(false);
-        return;
-      }
-
-      AuthService.register(
-        email,
-        name,
-        surname,
-        password,
-        phoneNumber,
-        photo
-      ).then(
-        () => {
-          navigate("/home");
-          window.location.reload();
-        },
-        (error) => {
-          const resMessage =
-            (error.response &&
-              error.response.data &&
-              error.response.data.message) ||
-            error.message ||
-            error.toString();
-
-          setMessage(resMessage);
-          setSuccessful(false);
-        }
-      );
-    }
-  };
-
   return (
     <div className="register-wrapper">
       <img src={ConnectInLogo} alt="ConnectIn Logo" className="connectInLogo" />
-      <div style={{ position: "relative" }}>
-        <h2 className="register-message">
-          Make the most of your professional life
-        </h2>
-      </div>
-      <Form onSubmit={handleRegister} ref={form}>
-        {!successful && (
-          <>
-            <MDBContainer>
-              <MDBRow>
-                <MDBCol
-                  md="10"
-                  lg="6"
-                  className="order-2 order-lg-1 d-flex flex-column align-items-center"
-                >
-                  <div className="d-flex flex-row align-items-center mb-4 m-3">
-                    <MDBIcon fas icon="envelope me-3" size="lg" />
-                    <MDBInput
-                      label="Your Email"
-                      id="form2"
-                      type="email"
-                      value={email}
-                      onChange={onChangeEmail}
-                      validators={{ requiredField, validEmail }}
-                    />
-                  </div>
-
-                  <div className="d-flex flex-row align-items-center mb-4 m-3">
-                    <MDBIcon fas icon="user me-3" size="lg" />
-                    <MDBInput
-                      label="Your Name"
-                      id="form1"
-                      type="text"
-                      className="w-100"
-                      value={name}
-                      onChange={onChangeName}
-                      validators={{ requiredField, validName }}
-                    />
-                  </div>
-
-                  <div className="d-flex flex-row align-items-center mb-4 m-3">
-                    <MDBIcon fas icon="id-card me-3" size="lg" />
-                    <MDBInput
-                      label="Your Surname"
-                      id="form1"
-                      type="text"
-                      className="w-100"
-                      value={surname}
-                      onChange={onChangeSurname}
-                      validators={{ requiredField, validSurname }}
-                    />
-                  </div>
-
-                  <div className="d-flex flex-row align-items-center mb-4 m-3">
-                    <MDBIcon fas icon="lock me-3" size="lg" />
-                    <MDBInput
-                      label="Password"
-                      id="form3"
-                      type="password"
-                      value={password}
-                      onChange={onChangePassword}
-                      validators={{ requiredField, validPassword }}
-                    />
-                  </div>
-
-                  <div className="d-flex flex-row align-items-center mb-4 position-relative m-3 ">
-                    <MDBIcon fas icon="key me-3" size="lg" />
-                    <MDBInput
-                      label="Repeat your password"
-                      id="form4"
-                      type="password"
-                      value={repeatPassword}
-                      onChange={onChangeRepeatPassword}
-                    />
-                    {passwordMatchError && (
-                      <div
-                        className="text-danger position-absolute"
-                        style={{ top: "110%", right: "45px" }}
-                      >
-                        {passwordMatchError}
-                      </div>
-                    )}
-                  </div>
-
-                  <div className="d-flex flex-row align-items-center mb-4 m-3">
-                    <MDBIcon fas icon="phone me-3" size="lg" />
-                    <MDBInput
-                      label="Your Phone Number"
-                      id="form1"
-                      type="tel"
-                      className="w-100"
-                      value={phoneNumber}
-                      onChange={onChangePhoneNumber}
-                      validators={{ requiredField, validPhoneNumber }}
-                    />
-                  </div>
-
-                  <PhotoUpload onFileUpload={handleFileUpload} />
-
-                  <MDBBtn className="mb-4" size="lg">
-                    Register
-                  </MDBBtn>
-                </MDBCol>
-
-                <MDBCol
-                  md="10"
-                  lg="6"
-                  className="order-1 order-lg-2 d-flex align-items-center"
-                >
-                  <img
-                    src={register}
-                    alt="register"
-                    classname="register-photo"
-                  />
-                </MDBCol>
-              </MDBRow>
-            </MDBContainer>
-          </>
-        )}
-        {message && (
-          <div className="form-group">
-            <div
-              className={
-                successful ? "alert alert-success" : "alert alert-danger"
-              }
-              role="alert"
+      <h2 className="register-message">
+        Make the most of your professional life
+      </h2>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <MDBContainer>
+          <MDBRow>
+            <MDBCol
+              md="10"
+              lg="6"
+              className="order-2 order-lg-1 d-flex flex-column align-items-center"
             >
-              {message}
-            </div>
-          </div>
-        )}
-      </Form>
+              <MDBInput
+                size="lg"
+                wrapperClass="mb-4"
+                label="Your Email"
+                id="form2"
+                type="email"
+                {...formRegister("email", { required: "Email is required" })}
+              />
+              {errors.email && (
+                <div className="invalid-feedback d-block">
+                  {errors.email.message}
+                </div>
+              )}
+
+              <MDBInput
+                size="lg"
+                wrapperClass="mb-4"
+                label="Your Name"
+                id="form1"
+                type="text"
+                {...formRegister("name", {
+                  required: "Name is required",
+                  minLength: {
+                    value: 3,
+                    message: "Name must be at least 3 characters long",
+                  },
+                  maxLength: {
+                    value: 20,
+                    message: "Name must be less than 20 characters long",
+                  },
+                })}
+              />
+              {errors.name && (
+                <div className="invalid-feedback d-block">
+                  {errors.name.message}
+                </div>
+              )}
+
+              <MDBInput
+                size="lg"
+                wrapperClass="mb-4"
+                label="Your Surname"
+                id="form1"
+                type="text"
+                {...formRegister("surname", {
+                  required: "Surname is required",
+                  minLength: {
+                    value: 3,
+                    message: "Surname must be at least 3 characters long",
+                  },
+                  maxLength: {
+                    value: 20,
+                    message: "Surname must be less than 20 characters long",
+                  },
+                })}
+              />
+              {errors.surname && (
+                <div className="invalid-feedback d-block">
+                  {errors.surname.message}
+                </div>
+              )}
+
+              <MDBInput
+                size="lg"
+                wrapperClass="mb-4"
+                label="Password"
+                id="form3"
+                type="password"
+                {...formRegister("password", {
+                  required: "Password is required",
+                  minLength: {
+                    value: 6,
+                    message: "Password must be at least 6 characters long",
+                  },
+                  maxLength: {
+                    value: 20,
+                    message: "Password must be less than 20 characters long",
+                  },
+                })}
+              />
+              {errors.password && (
+                <div className="invalid-feedback d-block">
+                  {errors.password.message}
+                </div>
+              )}
+
+              <MDBInput
+                size="lg"
+                wrapperClass="mb-4"
+                label="Repeat your password"
+                id="form4"
+                type="password"
+                {...formRegister("repeatPassword", {
+                  required: "Please confirm your password",
+                })}
+              />
+              {watch("password") !== watch("repeatPassword") && (
+                <div className="invalid-feedback d-block">
+                  Passwords do not match.
+                </div>
+              )}
+
+              <MDBInput
+                size="lg"
+                wrapperClass="mb-4"
+                label="Your Phone Number"
+                id="form1"
+                type="tel"
+                {...formRegister("phoneNumber", {
+                  required: "Phone number is required",
+                  pattern: {
+                    value: /^[0-9]{10}$/,
+                    message: "Phone number must contain 10 digits",
+                  },
+                })}
+              />
+              {errors.phoneNumber && (
+                <div className="invalid-feedback d-block">
+                  {errors.phoneNumber.message}
+                </div>
+              )}
+
+              <PhotoUpload onFileUpload={handleFileUpload} />
+
+              <MDBBtn
+                type="submit"
+                size="lg"
+                className="mb-4"
+                disabled={loading}
+              >
+                {loading && (
+                  <MDBSpinner className="mx-2" size="sm" color="secondary">
+                    <span className="visually-hidden"></span>
+                  </MDBSpinner>
+                )}
+                <span>Register</span>
+              </MDBBtn>
+              {message && (
+                <div className="form-group">
+                  <div className="alert alert-danger" role="alert">
+                    {message}
+                  </div>
+                </div>
+              )}
+            </MDBCol>
+
+            <MDBCol
+              md="10"
+              lg="6"
+              className="order-1 order-lg-2 d-flex align-items-center"
+            >
+              <img src={register} alt="register" className="register-photo" />
+            </MDBCol>
+          </MDBRow>
+        </MDBContainer>
+      </form>
     </div>
   );
 };
