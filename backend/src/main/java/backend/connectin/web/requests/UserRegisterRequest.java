87d33f5f71f96322959ce5a14a932080e4d91279
Panagiotis <p.kontoeidis@logicea.com>
2024-08-29 20:23:36 +0300

    fixed uploading image problem

--- backend/src/main/java/backend/connectin/web/requests/UserRegisterRequest.java
+++ backend/src/main/java/backend/connectin/web/requests/UserRegisterRequest.java
@@ -1,5 +1,7 @@
 package backend.connectin.web.requests;
 
+import org.springframework.web.multipart.MultipartFile;
+
 public class UserRegisterRequest {
 
     private String email;
@@ -7,6 +9,17 @@ public class UserRegisterRequest {
     private String firstName;
     private String lastName;
     private String phoneNumber;
+    private MultipartFile profilePicture;
+
+
+    public UserRegisterRequest(String email, String password, String lastName, String firstName, MultipartFile profilePicture, String phoneNumber) {
+        this.email = email;
+        this.password = password;
+        this.lastName = lastName;
+        this.firstName = firstName;
+        this.profilePicture = profilePicture;
+        this.phoneNumber = phoneNumber;
+    }
 
     public String getEmail() {
         return email;
@@ -46,5 +59,13 @@ public class UserRegisterRequest {
 
     public void setPhoneNumber(String phoneNumber) {
         this.phoneNumber = phoneNumber;
+    }
+
+    public MultipartFile getProfilePicture() {
+        return profilePicture;
+    }
+
+    public void setProfilePicture(MultipartFile profilePicture) {
+        this.profilePicture = profilePicture;
     }
 }
