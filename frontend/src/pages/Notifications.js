e892c89fa6263ceaabf5b7c23c9616a479e99ccf
Panagiotis <p.kontoeidis@logicea.com>
2024-08-30 22:35:11 +0300

    navigation bar styled

--- frontend/src/pages/Notifications.js
+++ frontend/src/pages/Notifications.js
@@ -2,7 +2,7 @@ import React from "react";
 import NotificationComponent from "../components/NotificationComponent";
 export default function Profile() {
   return (
-    <div style={{ backgroundColor: "#f0f0f0", minHeight: "100vh" }}>
+    <div style={{ backgroundColor: "#f3f2ef", minHeight: "100vh" }}>
       <NotificationComponent />
     </div>
   );
--- frontend/src/pages/Profile.js
+++ frontend/src/pages/Profile.js
@@ -2,7 +2,7 @@ import React from "react";
 import ProfileComponent from "../components/ProfileComponent";
 export default function Profile() {
   return (
-    <div style={{ backgroundColor: "#f0f0f0", minHeight: "100vh" }}>
+    <div style={{ backgroundColor: "#f3f2ef", minHeight: "100vh" }}>
       <ProfileComponent />
     </div>
   );
