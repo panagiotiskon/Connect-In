df60f206071b07bd37aefebda95729c1977345ed
Panagiotis Kontoeidis <panos.kontoeidis@gmail.com>
2024-09-12 19:03:02 +0300

    home page feed

---
+++ frontend/src/api/PostApi.js
@@ -0,0 +1,24 @@
+import axios from "axios";
+import AuthService from "../api/AuthenticationAPI";
+
+const API_URL = "https://localhost:8443";
+
+const getFeed = async (userId) => {
+  try {
+    const user = await AuthService.getCurrentUser();
+    const response = await axios.get(`${API_URL}/${user?.id}/feed`, {
+        headers: {
+          "Content-Type": "application/json",
+        },});
+    return response.data; 
+  } catch (error) {
+    console.error("Error fetching user feed:", error);
+    throw error;
+  }
+};
+
+const PostService = {
+  getFeed,
+};
+
+export default PostService;
