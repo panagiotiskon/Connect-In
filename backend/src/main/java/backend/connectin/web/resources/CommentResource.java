bfb8b5918be7233a3f4144718716adcb458f17b6
Panagiotis Kontoeidis <panos.kontoeidis@gmail.com>
2024-09-13 14:44:14 +0300

    comments on posts

---
+++ backend/src/main/java/backend/connectin/web/resources/CommentResource.java
@@ -0,0 +1,52 @@
+package backend.connectin.web.resources;
+
+import java.time.Instant;
+
+public class CommentResource {
+
+    private Long id;
+    private String content;
+    private Instant createdAt;
+    private Long userId;
+    private String username;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public Instant getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(Instant createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public Long getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Long userId) {
+        this.userId = userId;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+}
