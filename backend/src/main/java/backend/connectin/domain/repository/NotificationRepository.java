4889f45c70e0183c0ec924430e2c5319c3d9626c
Stelios Dimitriadis <s.dimitriadis@logicea.com>
2024-09-13 23:58:24 +0300

    finished notifications for connections

---
+++ backend/src/main/java/backend/connectin/domain/repository/NotificationRepository.java
@@ -0,0 +1,14 @@
+package backend.connectin.domain.repository;
+
+import backend.connectin.domain.Notification;
+import backend.connectin.domain.enums.NotificationType;
+import org.aspectj.weaver.ast.Not;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface NotificationRepository extends JpaRepository<Notification, Long> {
+    List<Notification> getNotificationsByUserId(long userId);
+}
