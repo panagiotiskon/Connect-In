831a8aef861d51f7d8e3480baceb507da8c53df0
Stelios Dimitriadis <s.dimitriadis@logicea.com>
2024-09-14 23:43:47 +0300

    created chat

--- backend/src/main/resources/db/migration/V13__create_messages_table.sql
+++ backend/src/main/resources/db/migration/V13__create_messages_table.sql
@@ -2,7 +2,7 @@ CREATE TABLE IF NOT EXISTS messages (
     id BIGINT AUTO_INCREMENT PRIMARY KEY,
     sender_id BIGINT NOT NULL,
     receiver_id BIGINT NOT NULL,
-    content TEXT NOT NULL,
+    content TEXT ,
     sent_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     FOREIGN KEY (sender_id) REFERENCES users(id) ON DELETE CASCADE,
     FOREIGN KEY (receiver_id) REFERENCES users(id) ON DELETE CASCADE
