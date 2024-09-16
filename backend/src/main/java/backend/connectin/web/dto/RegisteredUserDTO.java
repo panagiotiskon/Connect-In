f223a6958fc4fac2110a12382a75c2886942e6b9
Stelios Dimitriadis <s.dimitriadis@logicea.com>
2024-09-16 22:05:14 +0300

    fixed pending user connections

---
+++ backend/src/main/java/backend/connectin/web/dto/RegisteredUserDTO.java
@@ -0,0 +1,3 @@
+package backend.connectin.web.dto;
+
+public record RegisteredUserDTO (long userId,String firstName,String lastName,String jobTitle, String companyName,String profilePic,String profileType){}
