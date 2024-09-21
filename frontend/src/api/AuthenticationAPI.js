65a280434fad5205e85a6c0e2309e4a1c43acffb
Panagiotis Kontoeidis <panos.kontoeidis@gmail.com>
2024-09-21 13:48:52 +0300

    bugs

--- frontend/src/api/AuthenticationAPI.js
+++ frontend/src/api/AuthenticationAPI.js
@@ -68,13 +68,14 @@ const changePassword = async (oldPassword, newPassword) => {
 
 const login = (email, password) => {
     return axios
-        .post(API_URL + "/login", {email, password})
+        .post(API_URL + "/login", { email, password })
         .then((response) => {
             return response.data;
         })
         .catch((error) => {
             console.error("Login Error:", error.message);
             console.error("Error Config:", error.config);
+            throw error; 
         });
 };
 
