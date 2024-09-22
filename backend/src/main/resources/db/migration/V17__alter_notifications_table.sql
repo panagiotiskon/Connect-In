a8db24b8a3ea1b012858877e3723b40e3567288c
Panagiotis Kontoeidis <panos.kontoeidis@gmail.com>
2024-09-22 17:14:27 +0300

    dataset for jobs

--- backend/src/main/resources/db/migration/V17__alter_notifications_table.sql
+++ backend/src/main/resources/db/migration/V17__alter_notifications_table.sql
@@ -1,11 +1,8 @@
--- Step 1: Drop the foreign key constraint
 ALTER TABLE notification
     DROP FOREIGN KEY notification_ibfk_1;
 
--- Step 2: Drop the index on user_id
 ALTER TABLE notification
     DROP INDEX user_id;
 
--- (Optional) Step 3: Recreate the foreign key constraint (without the unique index)
 ALTER TABLE notification
     ADD CONSTRAINT notification_ibfk_1 FOREIGN KEY (user_id) REFERENCES users(id);
