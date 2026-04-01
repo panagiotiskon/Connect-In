20a5abb397f2ba93fc123656729f8ae3f85f4353
Panagiotis Kontoeidis <panos.kontoeidis@gmail.com>
2026-04-01 17:01:56 +0300

    ui fixes

--- backend/src/main/java/backend/connectin/web/controllers/AuthController.java
+++ backend/src/main/java/backend/connectin/web/controllers/AuthController.java
@@ -69,7 +69,7 @@ public class AuthController {
             @RequestParam("firstName") String firstName,
             @RequestParam("lastName") String lastName,
             @RequestParam("phoneNumber") String phoneNumber,
-            @RequestParam("profilePicture") MultipartFile profilePicture,
+            @RequestParam(value = "profilePicture", required = false) MultipartFile profilePicture,
             HttpServletResponse response) {
 
         try {
